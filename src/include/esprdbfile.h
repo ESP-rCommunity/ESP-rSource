756818c49c2e38c5a31895cdf599770272d44f3b
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2006-11-24 16:36:40 +0000

    Summary of changes:
    - This commit moves the common block apres (window pressure db)
      into a header file esprdbfile.h
    
    - While moving the common block change a further scan was made
      for references and in several locations the variables were 
      not used and were removed.
    
    - Found an file (mfprb1.F) where LAPRES was still within the
      AFN common block and corrected this.
    
    - To ensure that included variables in esp-r_dbfile.h are not
      hidden from casual view the phrase
    
    #include "esprdbfile.h"
    C esprdbfile.h supplies the following:
    C LAPRES,IFPRE (for window pressure databases)
    
      has been included once at the top of each file where esprdbfile.h
      is referenced.
    
    Testing summary:
    - Compiled Solaris F90/cc X11
    - Compiled Linux GCC 3.4 X11
    - A round of syntax checking was done (i.e. after a
      compile all of the souce code and libary code of every module
      of ESP-r was passed through forchk).
    - Ran the set of validation/QA/model and got identical
      results to that predicted on the 9th of November.
    - Opened up pressure coef database and saved successfully.
    - ran full Linux tester.pl with no differences against the
      development branch of 19 november.
    
    
    
    Former-commit-id: c6014a37e983303e2fe072fc25e0b0b5bb5a2347

---
+++ src/include/esprdbfile.h
@@ -0,0 +1,12 @@
+C This file is part of the ESP-r system.
+C Copyright Energy Systems Research Unit, University of
+C Strathclyde, Glasgow Scotland, 2001.
+
+C esprdbfile.h holds common block for database file names and units.
+
+C LAPRES - (72 char) wind pressure coefficients distribution database
+C ifpre  - file unit for wind pressure coef database
+      common/APRES/LAPRES,IFPRE
+      character LAPRES*72
+
+C end of esp-r_dbfile.h
