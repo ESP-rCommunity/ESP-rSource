e8dca4f67eac25e46db812b2ba2cfff7bf698eec
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2006-12-21 13:58:22 +0000

    This commit merges changes r683:715 on the sub-branch 'Jon_Hand' into 'development_branch'.
    Summary of changes:
    - The common blocks holding the file names and unit numbers
      of various databases have been relocated to an include file.
    - An error in res' reporting of surface absorbed solar radiation
      was corrected.
    - These changes have no impact upon ESP-r functionality (other
      than res' reporting on absorbed solar) and have no impact
      upon simulation results.
    
    
    Testing summary:
    - Successfully compiled GTK and X11 versions of ESP-r on Linux
      (GCC 3.4.6).
    
    
    All sub-branchs of 'development_branch' should now be synchronized with
    'development_branch'.  These actions are to be completed by the owners
    of these sub-branches following the instructions included in
    src/archive/subversion.trf.
    
    
    
    
    Former-commit-id: 0d22eed75cce8ca21159af117e005291922e008c

---
+++ src/include/esprdbfile.h
@@ -0,0 +1,50 @@
+C This file is part of the ESP-r system.
+C Copyright Energy Systems Research Unit, University of
+C Strathclyde, Glasgow Scotland, 2001.
+
+C esprdbfile.h holds common block for database file names and units.
+
+C LAPRES - (72 char) wind pressure coefficients distribution database
+C ifpre  - file unit for wind pressure coef database
+      common/APRES/LAPRES,IFPRE
+      character LAPRES*72
+
+C LOPTDB - (72 char) optical database file name
+C IOPTDB - unit number of optical database
+      COMMON/GOPTDB/LOPTDB,IOPTDB
+      character LOPTDB*72
+
+C LPCDB - (72 char) plant template database file name
+C IPCDB - unit number of plant template database
+      common/C7/LPCDB,IPCDB
+      character lpcdb*72
+
+C LPRFDB - (72 char) event profile database file name
+C IPRODB - unit number of event profile database
+      COMMON/PRODB/LPRFDB,IPRODB
+      character LPRFDB*72
+
+C LFCON - (72 char) materials db file name
+C LFMUL - (72 char) constructions db file name
+C IFCON - unit number of materials database
+C IFMUL - unit number of constructions database
+      common/CONDB/LFCON,IFCON,LFMUL,IFMUL
+      character LFCON*72,LFMUL*72
+
+C LCLIM - (72 char) climate file name associated with the model
+C ICLIM - unit number of climate file
+      COMMON/C22/ICLIM,LCLIM
+      character LCLIM*72
+
+C Default file names for databases (72 char):
+C DCLIM - default climate, DAPRES - default pressure coef
+C DFCON - default materials, DFMUL - default constructions
+C DOPTDB - default optics, DPRFDB - default events
+C DPCDB - default plant components
+      COMMON/DEFLT1/DCLIM,DAPRES,DFCON,DFMUL,DOPTDB,DPRFDB,DPCDB
+      character*72 DCLIM,DAPRES,DFCON,DFMUL,DOPTDB,DPRFDB,DPCDB
+
+C Define as integers.
+      integer ifpre,ioptdb,ipcdb,iprodb,ifcon,ifmul,iclim
+
+C end of esp-r_dbfile.h
