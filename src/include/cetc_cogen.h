25b2a449cdbf222a1e83010f2c62fab7fda68c20
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2007-06-06 18:13:24 +0000

    This commit merges changes r97:144, r219:221, r222:427, r462:463, r608:622, r681:699, r1043:1091, r1099:1109, and r1105:1106 on the sub-branch 'Annex42_combustion_CHP' into 'development_branch'.
    Summary of changes:
    - The implementation of the IEA/ECBCS Annex 42 combustion cogeneration
      model as a plant component has been completed.  This treats both
      internal combustion engine (ICE) and Stirling engine (SE) devices.
    - The full suite of inter-program comparative tests created for this
      model have been added to the 'additional_tests' directory.
    
    
    Testing summary: 
    - Successfully compiled X11 version of ESP-r on Linux 
      (GCC 3.4.6).
    - Ran tester.pl over complete test suite comparing Annex42_Combustion_CHP@r1109
      against development_branch@1109 using X11 version on Linux.  All test cases
      passed: no differences reported.
     -Ran the Forcheck static analyzer: no new errors were introduced and the
      total number of errors was reduced.
    
    
    All sub-branchs of 'development_branch' should now be synchronized with
    'development_branch'.  These actions are to be completed by the owners
    of these sub-branches following the instructions included in
    src/archive/subversion.trf.
    
    
    
    Former-commit-id: e50580ccd58bb88bdce7121e8567dee25e68fd24

--- src/include/cetc_cogen.h
+++ src/include/cetc_cogen.h
@@ -12,22 +12,21 @@ C.....Models supporting cetc_cogen.h interface
 
 C.....Named constants
 
-      integer iEngine_deactivated   ! Named constant indicating that a
-                                    !   high-level controller has requested
-                                    !   the engine be deactivated
-      integer iFuel_flow_controlled ! Named constant indicating the model's
-                                    !   fuel flow has been specified
-                                    !   by external controller
-      integer iPower_out_controlled ! Named constant indicating the model's
-                                    !   fuel flow has been specified
-                                    !   by external controller
-      integer iHeat_out_controlled  ! Named constant indicating model's
-                                    !   thermal output is controlled
+      integer iEngine_deactivated    ! Named constant indicating that a
+                                     !   high-level controller has requested
+                                     !   the engine be deactivated
+      integer iDimensionless_control ! Named constant indicating that the
+                                     !   controller is providing 
+      integer iPower_out_controlled  ! Named constant indicating the model's
+                                     !   fuel flow has been specified
+                                     !   by external controller
+      integer iHeat_out_controlled   ! Named constant indicating model's
+                                     !   thermal output is controlled
 
-      parameter ( iEngine_deactivated   = 0,
-     &            iFuel_flow_controlled = 1,
-     &            iPower_out_controlled = 2,
-     &            iHeat_out_controlled  = 3 )
+      parameter ( iEngine_deactivated    = 0,
+     &            iDimensionless_control = 1,
+     &            iPower_out_controlled  = 2,
+     &            iHeat_out_controlled   = 3 )
 
 
 
@@ -35,6 +34,7 @@ C.....Named constants
 C.....System operating modes
       integer iOP_inoperative         ! Unit is inoperative
       integer iOP_startup             ! Unit is starting-up
+      integer iOP_warmup              ! Unit is warming up
       integer iOP_normal_operation    ! Normal, active operation
       integer iOP_shutdown            ! Unit is shutting down
 
@@ -42,23 +42,31 @@ C.....System operating modes
 
       parameter ( iOP_inoperative        = 1,
      &            iOP_startup            = 2,
-     &            iOP_normal_operation   = 3,
-     &            iOP_shutdown           = 4 )
+     &            iOP_warmup             = 3,
+     &            iOP_normal_operation   = 4,
+     &            iOP_shutdown           = 5 )
 
 
 C.....System error flags
       integer iStatus_Normal                 ! normal operation
       integer iStatus_Fuel_fault             ! fuel shortage
       integer iStatus_CW_fault               ! cooling water is too hot
+      integer iStatus_Corr_Err               ! correlation error 
       parameter ( iStatus_Normal     = 0,
      &            iStatus_Fuel_fault = 1,
-     &            iStatus_CW_fault   = 2 )
+     &            iStatus_CW_fault   = 2,
+     &            iStatus_Corr_Err   = 3)
 
 
+C.....Cooldown mode
+      integer iCM_mandatory
+      integer iCM_optional
+      parameter ( iCM_mandatory = 1,
+     &            iCM_optional  = 2 )
 
 C.....Misc. Array dimensions
       integer iOP_Mode_Count
-      parameter ( iOP_Mode_Count = 4 )
+      parameter ( iOP_Mode_Count = 5 )
 
 
 C.....Close to zero tolerance
