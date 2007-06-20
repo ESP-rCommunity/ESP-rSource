87e5458c7eafd58a0bde8a500a3da318b54dcf5b
gkoko <gkoko@7d53e970-de11-0410-8a54-3d01b9da36cf>
2007-06-20 19:06:13 +0000

    This commit is a merge from development branch of version 1080 to 1260 This brings the Georgios_Kokogiannakis branch up to revision 1260.
    Former-commit-id: 4d64f432dd523d63dfd6c358b1564587f08cd0a7

--- src/include/chemical_properties.h
+++ src/include/chemical_properties.h
@@ -11,9 +11,54 @@ C
 C----------------------------------------------------------------------
 C     Named Constants.
 C----------------------------------------------------------------------
-C.....Number of known compounds
+C.....Number of elements in the periodic table
+      integer iElement_count
+      parameter ( iElement_count = 118 )
+
+C-----------------------------------------------------------------------
+C     Element atomic numbers. Add more as required
+C-----------------------------------------------------------------------
+      integer iElement_Carbon, iElement_Hydrogen
+      integer iElement_Oxygen, iElement_Nitrogen
+      parameter (iElement_Hydrogen = 1  )
+      parameter (iElement_Carbon   = 6  )
+      parameter (iElement_Nitrogen = 7  )
+      parameter (iElement_Oxygen   = 8  )
+
+C.....Number of active elements
+      integer iActive_El_count
+      parameter ( iActive_El_count  = 4 )
+
+C.....Indicies of active elements
+      integer iActive_elements(iActive_El_count)
+      integer iElc
+      data (iActive_elements(iElc), iElc=1,iActive_El_count)
+     &                     / iElement_Hydrogen,
+     &                       iElement_Carbon,
+     &                       iElement_Nitrogen,
+     &                       iElement_Oxygen     /
+
+      save iActive_elements
+      
+C-----------------------------------------------------------------------
+C     Elemental Molar masses(kg/kmol). Source: Van Wylen et al. 1994
+C-----------------------------------------------------------------------
+C.....Molar masses of all ('active') elements
+      real fElement_Molar_mass(115)
+      data fElement_Molar_mass(iElement_Hydrogen)   /  1.00794  /
+      data fElement_Molar_mass(iElement_Carbon)     / 12.011    /
+      data fElement_Molar_mass(iElement_Nitrogen)   / 14.0067   /
+      data fElement_Molar_mass(iElement_Oxygen)     / 15.994    /
+
+      save fElement_Molar_mass
+
+
+C-----------------------------------------------------------------------
+C     Named constants for compounds
+C-----------------------------------------------------------------------      
+C.....Number of supported compounds
       integer iCompound_count
-      parameter ( iCompound_count = 13 )
+      parameter ( iCompound_count = 15 )
 
 C.....H2
       integer iHydrogen
@@ -55,57 +100,137 @@ C.....N2
       integer iNitrogen
       parameter ( iNitrogen = 13 )
 
-C.....Heating value
-      integer iLHV, iHHV
-      parameter (iLHV = 1, iHHV = 2)
+C.....Ethanol (C2H5OH)
+      integer iEthanol
+      parameter ( iEthanol = 14 )
+
+C.....Methanol (CH3OH)
+      integer iMethanol
+      parameter ( iMethanol = 15 )            
+
 
 C-----------------------------------------------------------------------
-C     Elemental Molar masses(kg/kmol). Source: Van Wylen et al. 1994
+C     Composition of each compound
 C-----------------------------------------------------------------------
-C.....(hydrogen, carbon, oxygen)
-      real fMM_H, fMM_C, fMM_O, fMM_N
-      parameter ( fMM_H = 1.00794  )
-      parameter ( fMM_C = 12.011   )
-      parameter ( fMM_O = 15.9994  )
-      parameter ( fMM_N = 14.0067  )
+      integer iCompound_Composition(iCompound_count,iElement_count)
+      integer iCPc
+      integer iVector_length
+      parameter ( iVector_length = iCompound_count * iElement_count )
+C.....First zero all arrays      
+      data ( ( iCompound_Composition(iCPc, iElc ),
+     &         iELc=1,iElement_count ),
+     &       iCPc = 1, iCompound_count
+     &     )
+     &     / iVector_length * 0 /
 
+C.....Now add elemental compositions     
+      data iCompound_Composition(iHydrogen,   iElement_Hydrogen ) /  2 /
+
+      data iCompound_Composition(iMethane,    iElement_Carbon   ) /  1 /
+      data iCompound_Composition(iMethane,    iElement_Hydrogen ) /  4 /
+
+      data iCompound_Composition(iEthane,     iElement_Carbon   ) /  2 /
+      data iCompound_Composition(iEthane,     iElement_Hydrogen ) /  6 /
+
+      data iCompound_Composition(iPropane,    iElement_Carbon   ) /  3 /
+      data iCompound_Composition(iPropane,    iElement_Hydrogen ) /  8 /
+
+      data iCompound_Composition(iButane,     iElement_Carbon   ) /  4 /
+      data iCompound_Composition(iButane,     iElement_Hydrogen ) / 10 /
+
+      data iCompound_Composition(iHexane,     iElement_Carbon   ) /  6 /
+      data iCompound_Composition(iHexane,     iElement_Hydrogen ) / 14 /
+
+      data iCompound_Composition(iPentane,    iElement_Carbon   ) /  5 /
+      data iCompound_Composition(iPentane,    iElement_Hydrogen ) / 12 /
+
+      data iCompound_Composition(iC_Monoxide, iElement_Carbon   ) /  1 /
+      data iCompound_Composition(iC_Monoxide, iElement_Oxygen   ) /  1 /
+
+      data iCompound_Composition(iC_Dioxide,  iElement_Carbon   ) /  1 /
+      data iCompound_Composition(iC_Dioxide,  iElement_Oxygen   ) /  2 /
+
+      data iCompound_Composition(iOxygen,     iElement_Oxygen   ) /  2 /
+
+      data iCompound_Composition(iNitrogen,   iElement_Nitrogen ) /  2 /
+
+      data iCompound_Composition(iWater_l,    iElement_Hydrogen ) /  2 /
+      data iCompound_Composition(iWater_l,    iElement_Oxygen   ) /  1 /
+      
+      data iCompound_Composition(iWater_v,    iElement_Hydrogen ) /  2 /
+      data iCompound_Composition(iWater_v,    iElement_Oxygen   ) /  1 /
+
+      data iCompound_Composition(iMethanol,   iElement_Carbon   ) /  1 /
+      data iCompound_Composition(iMethanol,   iElement_Hydrogen ) /  4 /
+      data iCompound_Composition(iMethanol,   iElement_Oxygen   ) /  1 /
+
+      data iCompound_Composition(iEthanol,    iElement_Carbon   ) /  2 /
+      data iCompound_Composition(iEthanol,    iElement_Hydrogen ) /  6 /
+      data iCompound_Composition(iEthanol,    iElement_Oxygen   ) /  1 /
+
+      save iCompound_Composition
+      
 C-----------------------------------------------------------------------
 C     Compound enthalpies of formation (J / kmol), @ 25oC, 100 kPa
 C     Source: Van Wylen et al. 1994
 C-----------------------------------------------------------------------
-      real fEF_Water_l, fEF_Water_v
-      real fEF_Hydrogen, fEF_Methane, fEF_Oxygen, fEF_Propane
-      real fEF_Ethane, fEF_Hexane, fEF_Pentane, fEF_C_Monoxide
-      real fEF_C_Dioxide, fEF_Butane, fEF_Nitrogen
+      real fEnthalpy_Formation ( iCompound_count )
 
-      parameter ( fEF_Water_v = -241.826E06 )
-      parameter ( fEF_Water_l = -285.830E06 )
-      parameter ( fEF_Hydrogen = 0.000 )
-      parameter ( fEF_Methane = - 74.873E06 )
-      parameter ( fEF_Ethane = - 84.740E06 )
-      parameter ( fEF_Propane = -103.900E06 )
-      parameter ( fEF_Butane = -126.200E06 )
-      parameter ( fEF_Pentane = -146.500E06 )
-      parameter ( fEF_Hexane = -167.300E06 )
-      parameter ( fEF_C_Monoxide = -110.527E06 )
-      parameter ( fEF_C_Dioxide = -393.522E06 )
-      parameter ( fEF_Oxygen = 0.000 )
-      parameter ( fEF_Nitrogen = 0.000 )
+      data fEnthalpy_Formation ( iWater_v    )  / -241.826e06 /
+      data fEnthalpy_Formation ( iWater_l    )  / -285.830e06 /
+      data fEnthalpy_Formation ( iHydrogen   )  /    0.       /
+      data fEnthalpy_Formation ( iMethane    )  / - 74.873e06 /
+      data fEnthalpy_Formation ( iEthane     )  / - 84.740e06 /
+      data fEnthalpy_Formation ( iPropane    )  / -103.900e06 /
+      data fEnthalpy_Formation ( iButane     )  / -126.200e06 /
+      data fEnthalpy_Formation ( iPentane    )  / -146.500e06 /
+      data fEnthalpy_Formation ( iHexane     )  / -167.300e06 /
+      data fEnthalpy_Formation ( iC_Monoxide )  / -110.527e06 /
+      data fEnthalpy_Formation ( iC_Dioxide  )  / -393.522e06 /
+      data fEnthalpy_Formation ( iOxygen     )  /    0.       /
+      data fEnthalpy_Formation ( iNitrogen   )  /    0.       /
+      data fEnthalpy_Formation ( iMethanol   )  / -201.300e06 /
+      data fEnthalpy_Formation ( iEthanol    )  / -235.000e06 /
+          
+      save fEnthalpy_Formation
 
+
+C---------------------------------------------------------------------
+C     Flag indicating if compound can be oxidized (easily...)
+C---------------------------------------------------------------------
+      logical bIs_Oxidizable(iCompound_count)
+
+      data bIs_Oxidizable( iWater_v    )  / .false. /
+      data bIs_Oxidizable( iWater_l    )  / .false. /
+      data bIs_Oxidizable( iHydrogen   )  / .true.  /
+      data bIs_Oxidizable( iMethane    )  / .true.  /
+      data bIs_Oxidizable( iEthane     )  / .true.  /
+      data bIs_Oxidizable( iPropane    )  / .true.  /
+      data bIs_Oxidizable( iButane     )  / .true.  /
+      data bIs_Oxidizable( iPentane    )  / .true.  /
+      data bIs_Oxidizable( iHexane     )  / .true.  /
+      data bIs_Oxidizable( iC_Monoxide )  / .true.  /
+      data bIs_Oxidizable( iC_Dioxide  )  / .false. /
+      data bIs_Oxidizable( iOxygen     )  / .false. /
+      data bIs_Oxidizable( iNitrogen   )  / .false. /
+      data bIs_Oxidizable( iMethanol   )  / .true.  /
+      data bIs_Oxidizable( iEthanol    )  / .true.  /
+
+      save bIs_Oxidizable
 
 C---------------------------------------------------------------------
 C     Universial Ideal gas constant (J/kMol K)
 C---------------------------------------------------------------------
       real fR_universal
       parameter (fR_universal = 8314.0 )
-      
-      
+
+
 C---------------------------------------------------------------------
 C     Specific heat correlation methods:
 C---------------------------------------------------------------------
       integer iCp_Kyle_Corr           ! Kyle 1994 correlations
       parameter (iCp_Kyle_Corr = 1)
-
+      
 C.....Associate correlations with compounds (presently, only Kyle's
 C.....correlations are supported.
       integer iCp_Corr_method ( iCompound_count )
@@ -126,7 +251,7 @@ C---------------------------------------------------------------------
 C.....Correlation coefficients
       real fCp_Kyle_Coeff(iCompound_count, 4)
       save fCp_Kyle_Coeff
-      integer iCPc
+
       
       data (fCp_Kyle_Coeff ( iNitrogen, iCPc ),iCPc=1,4)
      &        / 28.90, -0.1571e-02, 0.8081e-05, -2.873e-09   /
@@ -164,6 +289,12 @@ C.....Correlation coefficients
       data (fCp_Kyle_Coeff ( iHexane, iCPc ),iCPc=1,4)
      &        /  6.938, 55.22e-02, -28.65e-05, 57.69e-09  /
 
+      data (fCp_Kyle_Coeff ( iMethanol, iCPc ),iCPc=1,4)
+     &        /  19.0, 9.152e-02, -1.22e-05, -8.039e-09   /
+
+      data (fCp_Kyle_Coeff ( iEthanol, iCPc ),iCPc=1,4)
+     &        /  19.9 , 20.96e-02, -10.38e-05, 20.05e-09  /
+     
 
 C.....Kyle Correlation ranges
       real fCp_Kyle_Ranges (iCompound_count,2)
@@ -216,6 +347,21 @@ C.....Kyle Correlation ranges
      &     fCp_Kyle_Ranges ( iHexane, iCp_Range_MAX )
      &     / 260.0, 1500.0 /
 
+      data fCp_Kyle_Ranges ( iMethanol, iCp_Range_MIN ),
+     &     fCp_Kyle_Ranges ( iMethanol, iCp_Range_MAX )
+     &     / 273.0, 1000.0 /
+
+      data fCp_Kyle_Ranges ( iEthanol, iCp_Range_MIN ),
+     &     fCp_Kyle_Ranges ( iEthanol, iCp_Range_MAX )
+     &     / 273.0, 1500.0 /
+          
+C---------------------------------------------------------------------
+C     Miscellaneous named constants
+C---------------------------------------------------------------------
+C.....Heating value
+      integer iLHV, iHHV
+      parameter (iLHV = 1, iHHV = 2)
+     
 C---------------------------------------------------------------------
 C     Flags for warning / error messages
 C---------------------------------------------------------------------
