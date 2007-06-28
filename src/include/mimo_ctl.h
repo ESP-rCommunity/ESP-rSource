db52edc49eae16f39686050aac63bcd3a23f55ce
aferguso <aferguso@7d53e970-de11-0410-8a54-3d01b9da36cf>
2007-06-28 12:43:42 +0000

    This commit completes functionality necessary to parse MIMO control data.
    Former-commit-id: 2648fb2f8f7beea60815191fe52a4c6b533a6d48

--- src/include/mimo_ctl.h
+++ src/include/mimo_ctl.h
@@ -2,37 +2,116 @@ C-----------------------------------------------------------------------
 C     mimo_ctl.h: Header file for MIMO controls.
 C-----------------------------------------------------------------------
 
+      common/MIMO_ctl/bMIMOActive,fMIMOVersion
+
       common/MIMO_sensors/iSenCount, cSenName, iSenLoc, 
      &                    cSenProp
       
-      common/MIMO_actuators/iActCount, cActName, iActLoc, fActMIN,
-     &                      fActMAX   
+      common/MIMO_actuators/iActCount, cActName, iActLoc, cActVariable,
+     &                      fActMIN, fActMAX   
      
       
-!       common/MIMO_daytypes/
-!       common/MIMO_periods/
-!       common/MIMO_loops/
+      common/MIMO_daytypes/iDayCount, cDayName, 
+     &                     iDayStartDD, iDayStartMM, iDayStartYYYY, 
+     &                     iDayEnd DD,  iDayEnd MM,  iDayEnd YYYY, 
+     &                     iDayStartDOY, iDayEndDOY
+
+     
       
+      common/MIMO_periods/iPerCount, cPerName, iPerStartHr, iPerEndHr
+      
+      common/MIMO_loops/iLoopCount, cLoopName, cLoopLaw, iLoopLaw,
+     &                  iLoopPriority, 
+     &                  iLoopDayTypeCount, cLoopDayTypes, iLoopDayTypes,
+     &                  iLoopSensorCount, cLoopSensors, iLoopSensors,
+     &                  iLoopActuatorCount,cLoopActuators,
+     &                     iLoopActuators,
+     &                  iLoopDataCount, fLoopData
+
+
 C.....MAX numbers of sensors, actuators, periods, day-types, loops...      
-      integer mMIMOSen 
-      parameter ( mMIMOSen = 24 ) 
-      
-      integer mMIMOAct 
-      parameter ( mMIMOAct = 24 )       
+           
+      integer mMIMOList
+      parameter ( mMIMOList = 24 ) 
       
       integer mMIMOLoc
       parameter ( mMIMOLoc = 4 )
       
+      integer mMIMOLoop
+      parameter ( mMIMOLoop = 48 )
       
-C.....SENSOR parameters      
+      integer mMIMOData 
+      parameter ( mMIMOData = 64 )
+      
+      integer mMIMOControlLaws
+      parameter ( mMIMOControlLaws = 1 )
+      
+C.....Version info 
+      real fMIMOVersion                             ! Version #      
+      logical bMIMOActive                           ! Flag for MIMO
+      
+C.....SENSOR parameters
       integer       iSenCount                       ! # of sensors 
-      character*24  cSenName(mMIMOSen)              ! Sensor names
-      integer       iSenLoc(mMIMOSen,mMIMOLoc)      ! Sensor locatation data
-      character*12  cSenProp(mMIMOSen)              ! Sensed peoperty
+      character*24  cSenName(mMIMOList)             ! Sensor names
+      integer       iSenLoc(mMIMOList,mMIMOLoc)     ! Sensor locatation data
+      character*12  cSenProp(mMIMOList)             ! Sensed peoperty
       
-C.....Actuator parameters      
+C.....Actuator parameters
       integer       iActCount                       ! # of Actuators 
-      character*24  cActName(mMIMOAct)              ! Actuator names
-      integer       iActLoc(mMIMOAct,mMIMOLoc)      ! Actuator locatation data
-      real          fActMAX(mMIMOAct)               ! Actuator max 
-      real          fActMIN(mMIMOAct)               ! Actuator min
+      character*24  cActName(mMIMOList)             ! Actuator names
+      integer       iActLoc(mMIMOList,mMIMOLoc)     ! Actuator locatation data
+      character*24  cActVariable(mMIMOList)         ! Actuated variable 
+      real          fActMAX(mMIMOList)              ! Actuator max 
+      real          fActMIN(mMIMOList)              ! Actuator min
+      
+C.....DAY Types
+      integer iDayCount                             ! # days.
+      character*24 cDayName(mMIMOList)              ! Daytype names
+      integer iDayStartDD(mMIMOList)                ! Start day-of-month
+      integer iDayStartMM(mMIMOList)                ! Start month
+      integer iDayStartYYYY(mMIMOList)              ! Start year
+      integer iDayStartDOY(mMIMOList)               ! Start day-of-year
+      integer iDayEndDD(mMIMOList)                  ! End day-of-month
+      integer iDayEndMM(mMIMOList)                  ! End month
+      integer iDayEndYYYY(mMIMOList)                ! End year
+      integer iDayEndDOY(mMIMOList)                 ! END day of year. 
+      
+C.....PERIODS
+      integer iPerCount                             ! # periods
+      character*24 cPerName(mMIMOList)              ! Period name
+      integer iPerStartHr(mMIMOList)                ! Period start hour
+      integer iPerEndHr(mMIMOList)                  ! Period end hour 
+      
+C.....LOOPS
+      integer iLoopCount                            ! # Loops 
+      character*24 cLoopName(mMIMOLoop)             ! Loop names
+      character*24 cLoopLaw(mMIMOLoop)              ! Loop type (keyword)
+      integer      iLoopLaw(mMIMOLoop)              ! Loop law index
+      integer iLoopPriority(mMIMOLoop)              ! Priority of loop (1-10)
+      
+      integer iLoopDayTypeCount(mMIMOLoop)
+      character*24 cLoopDayTypes(mMIMOLoop,mMIMOList) ! Daytype (keyword)       
+      integer      iLoopDayTypes(mMIMOLoop,mMIMOList) ! Daytype (index)
+      
+      integer iLoopPeriodCount(mMIMOLoop)
+      character*24 cLoopPeriods(mMIMOLoop,mMIMOList)   ! Periods (keyword)
+      integer      iLoopPeriods(mMIMOLoop,mMIMOList)   ! Periods (index)
+      
+      integer iLoopSensorCount(mMIMOLoop)
+      character*24 cLoopSensors(mMIMOLoop,mMIMOList)   ! Loop sensors (keyword)
+      integer      iLoopSensors(mMIMOLoop,mMIMOList)   ! Loop sensors (index)
+      
+      integer iLoopActuatorCount(mMIMOLoop)
+      character*24 cLoopActuators(mMIMOLoop,mMIMOList) ! Loop Actuators (keyword)
+      integer      iLoopActuators(mMIMOLoop,mMIMOList) ! Loop Actuators (index)
+      
+      integer iLoopDataCount(mMIMOLoop)               ! # of data in loop
+      real    fLoopData(mMIMOLoop,mMIMOData)          ! Data associated with loop
+      
+C.....CONTROLS
+      character*32 cControlLawNames(mMIMOControlLaws)   ! Names of supported controls
+      integer iMIMOLawCount                     ! number of supported controls
+      parameter ( iMIMOLawCount = 1 )
+      
+      data cControlLawNames / 'AIMS_Control' /
+      
