7a221e43082502cc80e210724e6ce34a3533708d
aferguso <aferguso@7d53e970-de11-0410-8a54-3d01b9da36cf>
2007-07-10 14:15:25 +0000

    This commit modifies the HOT3000 test case models to use the binary databases in /usr/esru/
    Former-commit-id: e096f04611951a449c8d2a47faa9dc10ef468ddc

--- tester/test_suite/HOT3000_test_cases/convert_for_unix.sh
+++ tester/test_suite/HOT3000_test_cases/convert_for_unix.sh
@@ -11,6 +11,8 @@ rm -fr  ./*/cfg/*.res
 ls -1 ./*/cfg/*.cfg | while read cfgfile; do
 
   echo " - editing $cfgfile ";
-  
+  perl -pi -e "s/\*pdb(\s+)\.\.\/\.\.\/databases\/plantc\.db1/*pdb\1\/usr\/esru\/esp-r\/databases\/plantc.db1/g" $cfgfile
+  perl -pi -e "s/\*evn(\s+)\.\.\/\.\.\/databases\/profiles\.db1/*evn\1\/usr\/esru\/esp-r\/databases\/profiles.db1/g" $cfgfile
+  perl -pi -e "s/\*clm(\s+).+$/*clm\1\/usr\/esru\/esp-r\/climate\/CAN_ON_Toronto_CWEC/g" $cfgfile
 
 done 
