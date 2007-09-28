5714602fd38b10bcc7a294ea4212a9147c736cf4
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2007-09-28 15:45:00 +0000

    This commit merges changes c1708 and c1744  on the sub-branch 'Jon_Hand' into 'development_branch'.
    Summary of changes:
    - Updated GTK code to support more font types so users
      can switch to Courier for reports in the text feedback
      area.
    - Updated GTK code so different sizes and types of fonts
      can be used in graphs and wire-frame views.
    
    
    Testing summary: 
    - Compile Solaris GCC 3.4 GTK
    - Compile Linux GCC 3.4 GTK
    - Compile Native windows GTK
    - validation/benchmark/QA run with no differences reported
    - tester.pl run with no additional differences reported (the
      midnight casual gain blip is still there in the development
      branch).
    
    
    All sub-branchs of 'development_branch' should now be synchronized with
    'development_branch'.  These actions are to be completed by the owners
    of these sub-branches following the instructions included in
    src/archive/subversion.trf.
    
    
    
    Former-commit-id: 8986e9ba32b61e847fa4f57884d946e5d8d93c96

--- src/lib/commons.h
+++ src/lib/commons.h
@@ -1,3 +1,4 @@
+
 /* Common block definitions as structs for direct access from C code.
    *** Note: definitions must be in _exactly_ the same order as in the
    ***  fortran code.
@@ -23,6 +24,35 @@
   COMMON/RAY2/ITDSP,ITBND,ITEPT,ITZNM,ITSNM,ITVNO,ITORG,ITSNR,
      &            ITOBS,ITHLS,ITHLZ,ITGRD,GRDIS,ITPPSW
 */
+
+/* Create an enum which will point to the Indices of the array called
+ * font_calculations_array[] 
+ */
+  enum{
+    serif_small,      /* 0 position in array is for Serif Small */
+    serif_medium,     /* 1 position in array is for Serif Medium */
+    serif_large,      /* 2 position in array is for Serif Large */
+    serif_largest,    /* 3 position in array is for Serif Largest */
+    courier_small,    /* 4 position in array is for Courier Small */
+    courier_medium,   /* 5 position in array is for Courier Medium */
+    courier_large,    /* 6 position in array is for Courier Large */
+    courier_largest}; /* 7 position in array is for Courier Largest */
+
+/* Create a structure called Font_Calculation_Variables that
+ * contains the variables needed for font calculations
+ */
+typedef struct  {
+
+   gint f_height;     // pixel height of default font 
+   gint f_width;      // pixel width of default font 
+ }Font_Calculation_Variables;
+ 
+ 
+/* Create an aray of Font_Calculation_Variables */
+
+Font_Calculation_Variables font_calculations_array[8]; 
+
+
 extern struct {
   long int ITDSP,ITBND,ITEPT,ITZNM,ITSNM,ITVNO,ITORG,ITSNR,ITOBS,ITHLS,ITHLZ,ITGRD;
   float GRDIS;
