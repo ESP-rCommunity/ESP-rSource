bd9380811ce25e1992cb170a973c7ef4e587caa1
ageissler <ageissler@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-01-28 07:47:43 +0000

    This commit brings branch Achim_Geissler up-to-date with development_branch up to and including r3833.
    Minor conflicts:
    - prjfmk.F: Repository version used
    - ESP_r_flat_file_description.odt: Repository version used
    - building.h: Repository version used, re-introduced new parameter MSPMRES; the current development_branch building.h seems to be a step backward from the previous one, somehow. Not sure whats going on.
    - h3k_report_data.F: Mine used (changes from hard-coded string lengths to variable string length def)
    
    Strangely, no conflicts for spmatl.f were encountered.
    
    
    
    
    Former-commit-id: ded2cd52952c0fabca0ef4c10d19a87f5b5e835b

--- src/include/building4262.h
+++ src/include/building4262.h
@@ -26,6 +26,7 @@ C Type declarations
       integer MGXYZ,MBTYP,MNM,MNRS,MNFA,MROW,MCOL,MLAY,MLEV,MGRID
       integer MMTR,MBND,MSNOD,MMAT,MIPVM,MIPVA,MSPS
       integer MDTY,MGTY,MGPER
+      integer MIMG
       
 C For the building:
       PARAMETER (MCOM=42)
@@ -103,7 +104,7 @@ C If MS changes, so should MEQ (MS+1) and MTR (MS+4).
       PARAMETER (MTR=66)
 
 C Multilayered construction limit
-      PARAMETER (MMLC=80)
+      PARAMETER (MMLC=120)
 
 C For the time-step controller:
       PARAMETER (MCVT1=6)
@@ -257,3 +258,6 @@ C less than or equal to MSPS.
 
 C Maximum number of simulation parameter sets.
       PARAMETER (MSPS=10)
+
+C Maximum number of images associated with model.
+      PARAMETER (MIMG=10)
