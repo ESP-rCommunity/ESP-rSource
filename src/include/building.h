9b7681379d471f2ef585f2725df899fcccba1f19
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-02-12 13:33:10 +0000

    This commit merges the changes c3490 of sub-branch 'Joe_Clarke' into 'development_branch'.  It also replaces src/building.h with the version at Joe_Clarke@3634.
    Summary of changes:
    - A number of <carriage return, line feed> characters have been
      replaced.
    - The building.h header file has been rationalized in order to
      better support parameter changes (e.g. number of zones and
      surfaces).  A number of variants of this file have been deleted
      as users should modify the revised version if they wish to
      compile the source to support larger models.
    
    
    Testing summary:
    - Compile checked (Linux f77).
    - A tester.pl comparison with the current development version passed.
    - Successfully compiled X11 version of ESP-r on Cygwin (gcc 3.4.4).
    
    
    All sub-branchs of 'development_branch' should now be synchronized with
    'development_branch'.  These actions are to be completed by the owners
    of these sub-branches following the instructions included in
    src/archive/subversion.trf.
    
    
    
    Former-commit-id: 5028b8b62e5b5d4999d5ac234ce4ab1a8c6101b9

--- src/include/building.h
+++ src/include/building.h
@@ -1,264 +1,165 @@
-C The following is a parameter file for building related facilites.
-C Note that if this is changed then both the esru libraries and ESRU
-C applications must be recompiled.
-C August 95 version.
-C Parameters misur and maxwin added 1.11.91
-C Parameters mcub, mgc,mpatch added 9.05.92
-C Parameters mgrt, mgtv added 31.03.95
-C 32 Surface version 07.00
-C 42 Surface version 05.03
-C 62 Surface version 06.04
-C 40 zone 62 surface version 03.05
-C 42 zone 62 surface version 04.07 updated
-C 52 zone 62 surface version 01.08 updated
+C ESP-r building-related definition file setting maximum
+C parameter values.
 
-C Used in the ESP suite of programs written by staff of the
-C Energy Simulation Research Unit of the University of Strathclyde.
+C Version: 52 zones, 62 surfaces/zone
+C          (modify via MCOM and MS respectively).
 
-C Type declarations
+C Type declarations.
       integer MCOM,MCON,MTV,MV,ME,MGP,MN,MP,MDY,MT,MA,MC,MBP,MTMS
       integer MTS,MGT,MCT,MTRACE,MGRDP,MLCOM,MDF,MDCFP,MDCFS,MDCFV
-      integer MTMC,MGAL,MANG,MGOPT,MISC,MSEN,MO,MF
-      integer MGRT,MGTV,MST, MS,MSM,MZS,MZRL,MEQ,MTR,MMLC,MCVT1
-      integer MCVT2,MSCH,IRWMAX,MB,MOX,MOZ,misur,MCUB,MGC,MPATCH
-      integer MTHF,MTHEQ,MDATA,MHCV,MFP,MSPMNOD,MSPMDAT,MVS,MGV,MLS
-      integer MLBS,MCVS,MEZ,MZE,MCZ,MSSZ,MZSS,MYSS,MSEZ,MZSE,MYSE
-      integer MEEZ,MXEE,MYEE,MECZ,MXEC,MYEC,MCVV,MNOD3,MCFF3,MNOD3T
-      integer MGXYZ,MBTYP,MNM,MNRS,MNFA,MROW,MCOL,MLAY,MLEV,MGRID
-      integer MMTR,MBND,MSNOD,MMAT,MIPVM,MIPVA,MSPS
-      integer MDTY,MGTY,MGPER
-      integer MIMG
-      
-C For the building:
-      PARAMETER (MCOM=52)
-      PARAMETER (MCON=2000)
-      PARAMETER (MTV=200)
-      PARAMETER (MV=24)
-      PARAMETER (ME=8)
-      PARAMETER (MGP=3)
-      PARAMETER (MN=35)
-      PARAMETER (MP=7)
-      PARAMETER (MDY=3)
-      PARAMETER (MT=24)
-      PARAMETER (MA=24)
-      PARAMETER (MBP=8)
+      integer MTMC,MGAL,MANG,MGOPT,MISC,MSEN,MO,MF,MGRT,MGTV,MST
+      integer MS,MSM,MZS,MZRL,MEQ,MTR,MMLC,MCVT1,MCVT2,MSCH,IRWMAX
+      integer MB,MOX,MOZ,MISUR,MCUB,MGC,MPATCH,MTHF,MTHEQ,MDATA,MHCV
+      integer MFP,MSPMNOD,MSPMDAT,MVS,MGV,MLS,MLBS,MCVS,MEZ,MZE,MCZ
+      integer MSSZ,MZSS,MYSS,MSEZ,MZSE,MYSE,MEEZ,MXEE,MYEE,MECZ
+      integer MXEC,MYEC,MCVV,MNOD3,MCFF3,MNOD3T,MGXYZ,MBTYP,MNM
+      integer MNRS,MNFA,MROW,MCOL,MLAY,MLEV,MGRID,MMTR,MBND,MSNOD
+      integer MMAT,MIPVM,MIPVA,MSPS,MDTY,MGTY,MGPER
+      integer MCNDV,MCNDC,MPICK,MVAR,MIMG
+
+C Geometry.
+      PARAMETER (MCOM=52)	!- Zones.
+      PARAMETER (MS=62)		!- Surfaces/zone (set MNSBZ in cfd.h to MS).
+      PARAMETER (MCON=2000)	!- Edges/zone.
+      PARAMETER (MTV=200)	!- Vertices/zone.
+      PARAMETER (MV=24)		!- Vertices/surface.
+      PARAMETER (MST=MS)	!- Used with view factors.
+      PARAMETER (MSM=MS+6)	!- Used with view factors.
+
+C Ground topology.
+      PARAMETER (MGRT=400)	!- Surfaces.
+      PARAMETER (MGTV=500)	!- Vertices.
+
+C Construction.
+      PARAMETER (ME=8)		!- Elements/construction.
+      PARAMETER (MN=35)		!- Nodes/construction.
+      PARAMETER (MGP=3)		!- Air gaps/construction.
+      PARAMETER (MMLC=120)	!- Multilayered constructions in model.
+      PARAMETER (MHCV=MN+ME)	!- Control volumes per construction.
+
+C Glazing.
+      PARAMETER (MGT=MS)	!- Insolated glazings in zone
+      PARAMETER (MTMC=7)	!- Glazing systems/zone, 
+      PARAMETER (MGAL=2)	!- Optical sets per optics db item. 
+      PARAMETER (MANG=9)	!- Angles at which optical data is held.
+      PARAMETER (MGOPT=90)	!- Set size in optics database.
+
+C Shading/insolation.
+      PARAMETER (MB=80)		!- Site obstruction blocks.
+      PARAMETER (MOX=50)	!- Grid lines in x-direction.
+      PARAMETER (MOZ=50)	!- Grid lines in z-direction.
+      PARAMETER (MISUR=10)	!- Surfaces insolated from one source.
+      PARAMETER (MBP=8)		!- TMC blind control periods.
+
+C Variable thermo-physical properties.
+      PARAMETER (MTHF=12)	!- Thermal functions.
+      PARAMETER (MTHEQ=3)	!- Thermal equations.
+      PARAMETER (MDATA=12)	!- Defining data items.
+
+C Special materials.
+      PARAMETER (MSPMNOD=40)	!- Special materials.
+      PARAMETER (MSPMDAT=21)	!- Defining data items.
+
+C Casual gains.
+      PARAMETER (MCT=7,MGTY=7)	!- Casual gain types.
+      PARAMETER (MDTY=10)	!- Day types.
+      PARAMETER (MGPER=72)	!- Periods.
+      PARAMETER (MC=24*MCT*MDTY)!- Total casual gains.
+
+C Mean radiant temperature.
+      PARAMETER (MCUB=4)	!- Sensors in zone.
+      PARAMETER (MGC=26000)	!- Surface grid cells.
+      PARAMETER (MPATCH=200)	!- Hemispherical patches.
+
+C Time.
+      PARAMETER (MP=7)		!- Days in week.
+      PARAMETER (MT=24)		!- Hours in day.
+      PARAMETER (MDY=3)		!- Day types.
+      PARAMETER (MA=24)		!- Air change periods.
+
+C Casual gain control.
+      PARAMETER (MLCOM=4)	!- Periods.
+      PARAMETER (MDF=9)		!- Daylight factor sets.
+      PARAMETER (MDCFP=5)	!- Daylight coefficient sets.
+      PARAMETER (MDCFS=2)
+      PARAMETER (MDCFV=145)	!- Sky patches.
+
+C Systems control.
+      PARAMETER (MSEN=4)
+      PARAMETER (MISC=9+8*MSEN)	!- Miscellaneous data items per control law
+      PARAMETER (MO=24)
+      PARAMETER (MF=100)
+      PARAMETER (MFP=4)		!- Fuzzy control periods.
+
+C Simulation control.
+      PARAMETER (MTRACE=60)	!- Trace parameters.
       PARAMETER (MTMS=1440)
       PARAMETER (MTS=1440)
+      PARAMETER (MGRDP=10)	!- Ground temperature profiles.
 
-C 'mgt' is the maximum number of insolated glazings and
-C this should be set to the same as MS. 
-      PARAMETER (MGT=62)
-      PARAMETER (MCT=3)
-      PARAMETER (MTRACE=60)
-      PARAMETER (MGRDP=10)
-
-C Casual gains limits.
-      PARAMETER (MDTY=10)
-      PARAMETER (MGTY=7)
-      PARAMETER (MGPER=72)
-
-C Allow for 24 (one per hour) * MDTY(day types) * 3 (casual gain types)
-      PARAMETER (MC=72*MDTY)
-
-C Casual gain control settings:
-      PARAMETER (MLCOM=4)
-      PARAMETER (MDF=9)
-      PARAMETER (MDCFP=5)
-      PARAMETER (MDCFS=2)
-      PARAMETER (MDCFV=145)
-
-C MTMC is number of different glazing systems per zone, 
-C MGAL is number of optical sets per optics db item 
-C TMC and MANG is the number of angles at which optical data is held, 
-C MGOPT is the maximum size of optic sets in optics database.
-      PARAMETER (MTMC=7,MGAL=2,MANG=9,MGOPT=90)
-
-C MISC is the maximum number of miscellaneous data
-C items for control laws.
-C Should be equal to 9 + 8*MSEN with a minimum of 16
-      PARAMETER (MISC=41)
-      PARAMETER (MSEN=4)
-      PARAMETER (MO=24)
-
-      PARAMETER (MF=100)
-
-C Ground topology
-      PARAMETER (MGRT=400)
-      PARAMETER (MGTV=500)
-
-C Change next 2 settings to increase/ reduce
-C number of surfaces. MSM is MS+6 for viewfactor
-C calculations. Also set MNSBZ in cfd.h to equal MS.
-      PARAMETER (MST=62)
-      PARAMETER (MS=62)
-      PARAMETER (MSM=68)
-
-C MZS is the number of items to report/select in res.
-      PARAMETER (MZS=90)
-
-C MZRL is associated with data taking up all fields of a zone
-C results library (i.e. is = nzrl). Should be larger of either MCOM or MS+12 or 36.
-      PARAMETER (MZRL=74)
-
-C If MS changes, so should MEQ (MS+1) and MTR (MS+4).
-      PARAMETER (MEQ=63)
-      PARAMETER (MTR=66)
-
-C Multilayered construction limit
-      PARAMETER (MMLC=120)
-
-C For the time-step controller:
+C Time-step control.
       PARAMETER (MCVT1=6)
       PARAMETER (MCVT2=2)
       PARAMETER (MSCH=24)
-      
-C For the time-step controller type 6:
-      PARAMETER (IRWMAX=20)
+      PARAMETER (IRWMAX=20)	!- For type 6.
 
-C For shading/ insolation:
-      PARAMETER (MB=80)
-      PARAMETER (MOX=50)
-      PARAMETER (MOZ=50)
+C Results analysis.
+      PARAMETER (MSPS=10)	!- Simulation parameter sets.
+      PARAMETER (MNFA=4)	!- Factorial analyses.
+      PARAMETER (MNRS=2**MNFA)	!- Result sets (set to 100 for sensitivity analysis).
+      PARAMETER (MIPVA=MSPS)	!- IPV assessments.
+      PARAMETER (MIPVM=12)	!- IPV metrics.
+      PARAMETER (MZS=90)	!- Number of items to report in res.
+      PARAMETER (MZRL=MS+12)	!- Fields in a zone results library record
+                                !  (minimum value is 36; if MCOM>MS use MCOM
+                                !  in formula).
+C Mathematical model.
+      PARAMETER (MEQ=MS+1)	!- Equations.
+      PARAMETER (MTR=MS+4)	!- Equation terms.
 
-C 'misur' is the maximum number of surfaces insolated from one source.
-      parameter (misur=10)
+C Multi-gridding.
+      PARAMETER (MVS=50)	!- Local vertices per surface.
+      PARAMETER (MGV=100)	!- Gridding vertices per surface.
+      PARAMETER (MLS=100)	!- Gridding lines per surface.
+      PARAMETER (MLBS=20)	!- Bounary lines per surface.
+      PARAMETER (MCVS=50)	!- Control volumes per surface.
+      PARAMETER (MEZ=50)	!- Edges per zone.
+      PARAMETER (MZE=ME+2)	!- Levels in edge.
+      PARAMETER (MCZ=10)	!- Corners per zone.
+      PARAMETER (MECZ=3*MCZ)	!- Edge-corner connections.
+      PARAMETER (MSSZ=20)	!- Surface-surface connections.
+      PARAMETER (MZSS=20)	!- z-intervals in surface-surface connections.
+      PARAMETER (MYSS=6*ME)	!- y-intervals in surface-surface connections.
+      PARAMETER (MSEZ=70)	!- Surface-edge connections.
+      PARAMETER (MZSE=20)	!- z-intervals in surface-edge connections.
+      PARAMETER (MYSE=6*ME)	!- y-intervals in surface-edge connections.
+      PARAMETER (MEEZ=30)	!- Co-linear edge-edge connection.
+      PARAMETER (MXEE=6*ME)	!- x-intervals in co-linear edge-edge connection.
+      PARAMETER (MYEE=6*ME)	!- y-intervals in co-linear edge-edge connection.
+      PARAMETER (MXEC=6*ME)	!- x-intervals in edge-corner connection.
+      PARAMETER (MYEC=6*ME)	!- y-intervals in edge-corner connection.
+      PARAMETER (MCVV=10)	!- Intersection vertices per control volume.
+      PARAMETER (MNOD3=1000)	!- Nodes in a 3D zone model.
+      PARAMETER (MCFF3=20*MNOD3)!- Zone matrix coefficients.
+      PARAMETER (MNOD3T=MNOD3+2)!- Cofficients in the temporary storage array.
+      PARAMETER (MGXYZ=15)	!- x, y and z intervals for ground.
+      PARAMETER (MBTYP=10)	!- Boundary types for ground.
+      PARAMETER (MNM=50)	!- Moisture nodes.
 
-C For mrt calculations:
-C 'mcub' is the number of MRT sensors in a zone.
-      PARAMETER (MCUB=4,MGC=26000,MPATCH=200)
-
-C For variable thermophysical properties:
-      PARAMETER (MTHF=12,MTHEQ=3,MDATA=12)
-
-C Maximum number of homogeneous control volumes per construction.
-      PARAMETER (MHCV=MN+ME)
-
-C For max no. of fuzzy control periods:
-      PARAMETER (MFP=4)
-
-C special materials parameters.
-      PARAMETER (MSPMNOD=40,MSPMDAT=16)
-
-C Multi-gridding:
-
-C Maximum number of local vertices per surface.
-      PARAMETER (MVS=50)
-
-C Maximum number gridding vertices per surface.
-      PARAMETER (MGV=100)
-
-C Maximum number of gridding lines per surface.
-      PARAMETER (MLS=100)
-
-C Maximum number of bounary lines per surface.
-      PARAMETER (MLBS=20)
-
-C Maximum number of control volumes per surface.
-      PARAMETER (MCVS=50)
-
-C Maximum number of edges per zone.
-      PARAMETER (MEZ=50)
-
-C Maximum number of levels in edge.
-      PARAMETER (MZE=ME+2)
-
-C Maximum number of corners per zone.
-      PARAMETER (MCZ=10)
-
-C Maximum number of surface-surface connections.
-      PARAMETER (MSSZ=20)
-
-C Maximum number of z-intervals in surface-surface connections.
-      PARAMETER (MZSS=20)
-
-C Maximum number of y-intervals in surface-surface connections.
-      PARAMETER (MYSS=6*ME)
-
-C Maximum number of surface-edge connections.
-      PARAMETER (MSEZ=70)
-
-C Maximum number of z-intervals in surface-edge connections.
-      PARAMETER (MZSE=20)
-
-C Maximum number of y-intervals in surface-edge connections.
-      PARAMETER (MYSE=6*ME)
-
-C Maximum number of co-linear edge-edge connection.
-      PARAMETER (MEEZ=30)
-
-C Maximum number of x-intervals in co-linear edge-edge connection.
-      PARAMETER (MXEE=6*ME)
-
-C Maximum number of y-intervals in co-linear edge-edge connection.
-      PARAMETER (MYEE=6*ME)
-
-C Maximum number of edge-corner connections.
-      PARAMETER (MECZ=3*MCZ)
-
-C Maximum number of x-intervals in edge-corner connection.
-      PARAMETER (MXEC=6*ME)
-
-C Maximum number of y-intervals in edge-corner connection.
-      PARAMETER (MYEC=6*ME)
-
-C Maximum number of intersection vertices per control volume.
-      PARAMETER (MCVV=10)
-
-C Maximum number of nodes in a 3D zone model.
-      PARAMETER (MNOD3=1000)
-
-C Maximum number of zone matrix coefficients (linked to
-C MNOD3 - pragmatic suggestion is MNOD3 times 20).
-      PARAMETER (MCFF3=20000)
-
-C Maximum number of cofficients in the temporary full storage array.
-      PARAMETER (MNOD3T=MNOD3+2)
-
-C Maximum number of X,Y,and Z - interval for ground.
-      PARAMETER (MGXYZ=15)
-
-C Maximum number of boundary types for ground.
-      PARAMETER (MBTYP=10)
-
-C Maximum number of moisture nodes.
-      PARAMETER (MNM=50)
-
-C For results analysis:
-C   Maximum number of result sets - should either 
-C   be 30 (default) or 100 for sensitivity analysis:
-      PARAMETER (MNRS=30)
-
-C For factorial analysis: MNRS >= 2^MNFA
-      PARAMETER (MNFA=4)
-
-C Structured-gridding:
-C Maximum number of rows, columns, layers.
-      PARAMETER (MROW=12,MCOL=12,MLAY=12)
-
-C Maximum number of rows, columns, and layers.
+C Structured-gridding.
+      PARAMETER (MROW=12)	!- Rows.
+      PARAMETER (MCOL=12)	!- Columns.
+      PARAMETER (MLAY=12)	!- Layers.
+      PARAMETER (MGRID=10*MROW*MCOL*MLAY)	!- Control volumes (should be less than MNOD3).
       PARAMETER (MLEV=12)
+      PARAMETER (MMTR=30)	!- Material geometries.
+      PARAMETER (MBND=30)	!- Surface boundary boxes.
+      PARAMETER (MSNOD=MNOD3/10)!- Surface nodes.
+      PARAMETER (MMAT=MCON*ME)	!- Materials.
 
-C Maximum number of control volumes ( should be less than MNOD3).
-      PARAMETER (MGRID=10*MROW*MCOL*MLAY)
-
-C Maximum number of material geometries.
-      PARAMETER (MMTR=30)
-
-C Maximum number of surface boundary boxes.
-      PARAMETER (MBND=30)
-
-C Maximum number of surface nodes.
-      PARAMETER (MSNOD=MNOD3/10)
-
-C Maximum number of materials
-      PARAMETER (MMAT=MCON*ME)
-
-C Maximum number of IPV metrics and assessments to be run (should be
-C less than or equal to MSPS.
-      PARAMETER (MIPVM=12,MIPVA=10)
-
-C Maximum number of simulation parameter sets.
-      PARAMETER (MSPS=10)
-
-C Maximum number of images associated with model.
-      PARAMETER (MIMG=10)
+C Miscellaneous.
+      PARAMETER (MCNDV=20)
+      PARAMETER (MCNDC=100)
+      PARAMETER (MPICK=30)
+      PARAMETER (MVAR=5)
+      PARAMETER (MIMG=10)	!- Images associated with model.
