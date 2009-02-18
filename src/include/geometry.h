960814ff42d67d6dcd3d7cc3b00fda761cf272f0
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-02-18 08:22:51 +0000

    - Move the common block G0 into geometry.h and update   the source to replace references to this common block   with include geometry.h statements.
    Testing
    - This commit Linux GCC 3.4 X11
    - Pre-commit syntax check with no additional warnings.
    - Interactive test loading a model.
    
    For commit 3894 did the following additional tests after commit.
    - Compile Cygwin GCC 3.4 X11
    - Compile MSYS GCC 3.4 GTK
    - Compile OSX PPC GCC 3.4 X11
    - Compile OSX Intel GCC 4.2 X11
    - Compile Linux GCC 3.4 X11
    - Compile Solaris Intel F90 X11
    - Interactive start of prj on each of the above.
    
    
    
    Former-commit-id: 815d48bee4359bda122bbb371732103c82ea473e

--- src/include/geometry.h
+++ src/include/geometry.h
@@ -2,6 +2,14 @@ C This header relates to geometric entities in ESP-r. It is
 C dependant on building.h and include statements to geometry.h should
 C follow building.h so that parameters will have been defined.
 
+C ctype (4 char) - zone shape type (REC, REG or GEN) see egomin for definition
+C gversion (real) version of the geometry file (1.0 legacy, 1.1 current).
+C igupgrade (integer) 0 = do nothing, 1 = leave at 1.0, 2 = upgrade to 1.1
+      character CTYPE*4
+      real gversion
+      integer igupgrade
+      common/g0/CTYPE(MCOM),gversion(MCOM),igupgrade
+
 C NGL - contains the number of default windows in each face. As this
 C       data structure is depreciated it should be removed.
       integer ngl        
