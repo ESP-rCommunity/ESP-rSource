f20f30f2d568b8a77cd6c8694c60252d72e4fa7d
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-02-18 17:16:54 +0000

    - Separate character and integer/real data for common   block g0 by placing numbers in common g2.
    Testing
    - Compile Linux GCC 3.4 X11
    - Pre-commit syntax check indicating 15-60 fewer
      warning messages for most esp-r applications.
    
    
    
    
    Former-commit-id: 44cf03c602e1d62f5efde8786c48ad3cf9672813

--- src/include/geometry.h
+++ src/include/geometry.h
@@ -3,16 +3,13 @@ C dependant on building.h and include statements to geometry.h should
 C follow building.h so that parameters will have been defined.
 
 C ctype (4 char) - zone shape type (REC, REG or GEN) see egomin for definition
+      character CTYPE*4
+      common/g0/CTYPE(MCOM)
+
 C gversion (real) version of the geometry file (1.0 legacy, 1.1 current).
 C igupgrade (integer) 0 = do nothing, 1 = leave at 1.0, 2 = upgrade to 1.1
-      character CTYPE*4
       real gversion
       integer igupgrade
-      common/g0/CTYPE(MCOM),gversion(MCOM),igupgrade
-
-C NGL - contains the number of default windows in each face. As this
-C       data structure is depreciated it should be removed.
-      integer ngl        
-      common/g2/ngl(MS)
+      common/g2/gversion(MCOM),igupgrade
 
 
