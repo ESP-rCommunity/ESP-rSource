41808d4789786df992e0e508330890512897ae36
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-02-19 14:37:36 +0000

    - Move definition of common block prec17 into the   header file geometry.h and adjust references within   the source code. Several changes were required in   esrures because the variable ZBASEA() is actually   included in a different subroutine RECV5. The common   recv5 was adjusted and references to geometry.h common   block prec17 were used instead.
      The changes in esrures will be added in a 2nd commit
      to follow in a few minutes.
    
    Testing
    - compile Linux GCC 3.4 X11
    - pre-commit syntax check
    - ran tester.pl with no differences reported.
    
    
    
    
    Former-commit-id: dc301e3f6d54a9236a9638cba4fe665b2d16d86b

--- src/include/geometry.h
+++ src/include/geometry.h
@@ -16,3 +16,13 @@ C Default solar distribution and shading directives.
       integer ndp,idpn
       common/g4/ndp(MCOM),idpn(MCOM,3)
 
+C ZBASEA is the floor area of the zone, IBASES is a list of surfaces
+C which make up the floor, IUZBASEA signals that the user selected
+C specific surfaces (two), has edited the floor area (one) or it was
+C calculated based on FLOR orientations (zero). IZBASELIST is the
+C number of items in the list. 
+      real zbasea
+      integer ibases,iuzbasea,izbaselist
+      common/prec17/zbasea(MCOM),ibases(MCOM,12),iuzbasea(MCOM),
+     &  izbaselist(MCOM)
+
