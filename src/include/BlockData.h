d879026bc68e98c4d716fa094f5433ec543e25b7
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-04-06 13:38:14 +0000

    This commit merges the changes c1781, r4081:4085, and r4121:4125 of the sub-branch 'Kamel_Haddad' into 'development_branch'.
    Summary of changes:
    - Upgrade to the natural convection heat exchanger plant component.
      The call to the coefficient generator of the model was not active
      before this change pending the upgrade to the model.
    - Add ASHRAE Standard 140 Furnace Test Cases to test suite.
    
    
    Testing summary:
    - Successfully compiled noX GTK and X11 versions of ESP-r on Linux
      (GCC 3.4.6 and 4.1.2).
    - Pre-commit and post-commit syntax checks on full source of all modules
    - Run three test cases and inspect results
    - Compile Linux GCC4 X11
    
    
    All sub-branchs of 'development_branch' should now be synchronized with
    'development_branch'.  These actions are to be completed by the owners
    of these sub-branches following the instructions included in
    src/archive/subversion.trf.
    
    
    
    Former-commit-id: a954840ae7d3f74aca0f6dbfd06fcaf48c3c6af6

---
+++ src/include/BlockData.h
@@ -0,0 +1,49 @@
+C===================== Block data ======================================
+C     Created by: Didier Thevenard
+C     Created on: April 2007
+C     Copyright:  CETC 2007
+C-----------------------------------------------------------------------
+C     ABSTRACT:
+C     Block data to initialize variables in the STRATIFIED_TANK and in 
+C     the NCHE commons
+C
+C=======================================================================
+
+      BLOCK DATA
+
+C---- Include commons
+
+      INCLUDE "stratified_tank.h"
+      INCLUDE "NCHE.h"
+
+C---- Initialize variables in the STRATIFIED_TANK common
+
+      INTEGER NAUX
+      PARAMETER(NAUX=MAX_NNODES*MAX_STANK)
+
+      DATA N_STANK/0/
+     &     IPCOMP_STANK/MAX_STANK*0./
+     &     TP_STANK/NAUX*0./
+     &     TF_STANK/NAUX*0./
+     &     TAVGP_STANK/MAX_STANK*0./
+     &     TAVGF_STANK/MAX_STANK*0./
+     &     PTIMEF_STANK/-9.9999E+10/
+
+C---- Initialize variables in the NCHE common
+
+      DATA N_NCHE/0/
+     &     IPCOMP_NCHE/MAX_NCHE*0/
+     &     fHX_eff_CR_a/MAX_NCHE*0./
+     &     fHX_eff_CR_b/MAX_NCHE*0./
+     &     fHX_flow_P_c/MAX_NCHE*0./
+     &     fHX_flow_P_d/MAX_NCHE*0./
+     &     fHX_height/MAX_NCHE*0./
+     &     fTank_Height/MAX_NCHE*0./
+     &     iStrat_tank_CompIndex/MAX_NCHE*0/
+     &     iStrat_tank_number/MAX_NCHE*0/
+
+C---- End of common
+
+      END
+
+
