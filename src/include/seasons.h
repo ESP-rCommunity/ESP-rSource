d923f71f5a2779cfb71eaf84ee8bf9f05fbf0237
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-05-30 02:37:14 +0000

    - Further steps to reduce differences between getmultip   and DDRADSUM (in preparation for shifting a large block   of code into a separate subroutine).
    - Added several more common blocks to include/seasons.h.
      This moves into common several local variables related
      to setting up degree day calculations so they are available
      to preserve in model files.
      
      Located a bug in one calculation which was using a week
      index rather than the julian day. 
      
      This commit to allow static syntax checks.
    
    Testing
    - Interactive tests with the clm module and IPV menus of
      prj. During this testing updated the Dundee entry in the
      climatelist file (revisions 4312) and then used it in 
      tests.  Clm and prj show the same reported values.
    
    
    
    
    Former-commit-id: 9487d68fd02438aaaae7e3fd61b01cb74a1a725a

--- src/include/seasons.h
+++ src/include/seasons.h
@@ -1,6 +1,6 @@
 C This header relates to climate season entities in ESP-r. It is
-C not dependant on other header files and include statements to
-C seasons.h can are not sensitive to placement.
+C dependant on header file building.h and include statements to
+C seasons.h should follow building.h.
 
 C Typical assessment period definitions.
       integer ia1wins,ia1winf ! early jan-feb period start and finish
@@ -20,3 +20,32 @@ C transition (i.e. mar-may & sep-oct) and one period for summer.
       integer is1sums,is1sumf ! summer season start and finish
       common/typsea/is1wins,is1winf,is2wins,is2winf,is1sprs,is1sprf,
      &              is2sprs,is2sprf,is1sums,is1sumf
+
+C When scanning climate data the average and total degree days for each
+C season are held in block ddseasonavg.
+      real seahddwk  ! hdd avg/week (for each season)
+      real seacddwk  ! cdd avg/week (for each season)
+      real seahddtot ! total hdd during each season
+      real seacddtot ! total cdd during each season
+      common/ddseasonavg/seahddwk(MSPS),seacddwk(MSPS),seahddtot(MSPS),
+     &                   seacddtot(MSPS)
+
+
+C For each of the seasons: 
+      real wkdiff   ! difference in the performance parameters for the
+                    ! assessment period and the season
+      integer iwkbest  ! index of the best week (1-52)
+      integer iwkbstrt ! the julian day start of the best week
+      real wkheatdd ! heating degree days in the seasons best week
+      real wkcooldd ! cooling degree days in the seasons best week.
+      common/wkseasondiff/wkdiff(MIPVA),iwkbest(MIPVA),iwkbstrt(MIPVA),
+     &                    wkheatdd(MIPVA),wkcooldd(MIPVA)
+
+C When calculating best fit periods user supplied weightings and
+C base temperatures.    
+      real hddw   ! weighting for heating dd
+      real cddw   ! weighting for cooling dd
+      real radw   ! weighting for solar radiation.
+      real hddbaset ! base heating temperature for dd calcs
+      real cddbaset ! base cooling temperature for dd calcs
+      common/seaddweighting/hddw,cddw,radw,hddbaset,cddbaset
