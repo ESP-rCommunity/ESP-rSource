50aebff55fdc836943d9047017419de15625cbdd
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2009-09-18 15:45:58 +0000

    - Move common block C4 into site.h and rename XLAT to sitelat   and XLON to sitelongdif.
    - Split SNFNAM into a separate common block C5SD so that common
      block C5 is all numerical data (to reduce syntax warnings).
    
    - Move common block C14 into site.h and update associated code
      blocks
    
    - Move common block ground into site.h
    
      Users will not notice. Further code tidy and documentation for
      developers. No change in predictions.  
    
    Testing
    - Syntax checks before and after to each common block move
    - Compile Linux GCC 4.1 X11
    - Ran tester.pl with no differences reported.
    
    
    
    
    Former-commit-id: f47b36a83caf55d3017619d660a4e291aee909dd

--- src/include/site.h
+++ src/include/site.h
@@ -2,6 +2,13 @@ C This header relates to site-related model entities in ESP-r. It is
 C dependant on building.h and should follow building.h so that
 C parameters will have been defined.
 
+C Site position.
+      real  sitelat  ! site latitude (degrees) 
+      real  sitelongdif  ! longitudinal difference between the site and the
+                  ! reference time meridian (east +ve)
+      COMMON/C4/sitelat,sitelongdif
+
+C Exposure of the building on the site as well as ground and snow reflection data.
       integer siteexposureindex  ! site exposure index :
                      ! 1= city centre, normal case; 2= urban site, normal case
                      ! 3= rural site, normal case;
@@ -18,12 +25,22 @@ C parameters will have been defined.
       real groundreflmonth ! no snow ground reflectivity for each month
       real snowgroundrefl  ! uncorrected snow covered ground reflectivity
       integer dayswithsnow  ! monthly number of days with snow on the ground
-      character*72 SNFNAM ! file containing hourly snow depth information
       COMMON/C5/siteexposureindex,groundrefl,groundreflmodel,
-     &  groundreflmonth(12),snowgroundrefl,dayswithsnow(12),SNFNAM
+     &  groundreflmonth(12),snowgroundrefl,dayswithsnow(12)
+
+      character*72 SNFNAM ! file containing hourly snow depth information
+      COMMON/C5SD/SNFNAM
 
 C Site viewfactors from building surfaces to the site.
       real skyview      ! viewfactor to the sky vault (0.0 - 1.0) 
       real groundview   ! viewfactor to the ground (0.0 - 1.0)
       real buildingview ! viewfactor to buildings (0.0 - 1.0)
       COMMON/C5R/skyview,groundview,buildingview
+
+C Monthly profiles of temperatures defined by the user and standard profiles.
+      real UGRDTP    ! user defined monthly temperature profile
+      integer NGRDP  ! numer of user defined monthly profiles
+      COMMON/C14/UGRDTP(12,MGRDP),NGRDP
+
+      real grdtmp ! standard (compiled in) monthly temperature profiles
+      common/ground/grdtmp(12,mgrdp)
