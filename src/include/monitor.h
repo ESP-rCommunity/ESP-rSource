ec4843174aadc26f5d0bd5e69f1c7f6e0252f45a
ageissler <ageissler@7d53e970-de11-0410-8a54-3d01b9da36cf>
2010-02-08 21:40:14 +0000

    Adapted parameter MDPH to MCOM. Not updating this parameter leads to boundary error in blibsv().
    Compile and run on OSX gcc 4.0.1 + gfortran O.K.
    
    No crash anymore
    
    
    
    Former-commit-id: 002a4a872140bf895dd9b459d3a9cbfe7393535f

--- src/include/monitor.h
+++ src/include/monitor.h
@@ -3,4 +3,4 @@ C mdph is the max data block per hour dumped to library file.
 C Should equal MCOM*60. Where 60 is maximum number of time-steps
 C per hour for the building.
       integer mdph
-      parameter (mdph=1500)
+      parameter (mdph=4320)
