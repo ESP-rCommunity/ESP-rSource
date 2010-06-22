63b0e52e75f65ee9b779d46bd3de6abe45c2e639
ibeausol <ibeausol@7d53e970-de11-0410-8a54-3d01b9da36cf>
2010-06-22 02:10:42 +0000

    This commit merges the changes r5347:5415 of the sub-branch 'pre-release_patches' into 'development_branch'.
    Summary of changes:
    - This commit incorporates a number of bug fixes and patches that were
      corrected during the pre-release regression testing.  Refer to the
      svn log of pre-release_patches for details.
    
    
    Testing summary:
    - Compiled on Cygwin with gcc 4.3.2.
    
    
    Former-commit-id: 08f19a9e462bef935d6f177324afcc58f9d2a837

--- src/validation/benchmark/UKNCM/test4_hotel/cfg/remove_ncm_stuff.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f *_typ.cfg *_ref.cfg *_not.cfg *_str.cfg *ncm1 *txt libb *res *not.cnn
-rm -f fort.* *.inp ../ctl/*_str.ctl ../nets/*str_temporal.tdfa
-rm -f ../zones/*_str.geo ../zones/*_str.con ../zones/*_str.tmc 
-rm -f ../zones/*_not.geo ../zones/*_not.con ../zones/*_not.tmc 
-rm -f ../zones/*_typ.geo ../zones/*_typ.con ../zones/*_typ.tmc 
-rm -f ../zones/*_str.geo ../zones/*_str.con ../zones/*_str.tmc 
