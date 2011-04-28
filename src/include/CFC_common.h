f5dad8034cb99216971ab856702bc7e3cf52684a
ageissler <ageissler@7d53e970-de11-0410-8a54-3d01b9da36cf>
2011-04-28 19:32:58 +0000

    C'tnd bug fix effort for CFC daylight calcs. Results are beginning to make sense. Further tests necessary!
    C & r on OS X O.K., tests looking promising.
    
    
    
    
    Former-commit-id: 358126007a5460a2633f31e34fcac72537c18211

--- src/include/CFC_common.h
+++ src/include/CFC_common.h
@@ -805,11 +805,13 @@ C Back diffuse-diffuse ground transmittance
 C---------------------------------------------------------------------------------
 C COMMONS FOR CFC type CONSTRUCTIONS 
       common/CFCsolarS/RadIncDirS(mcom,ms),RadIncDifS(mcom,ms),
+     &      RadIncDifSky(mcom,ms),RadIncDifGrd(mcom,ms),
      &      transBB_S(mcom,ms),transD_S(mcom,ms),refl_S(mcom,ms),
      &      transBB_V(mcom,ms),transD_V(mcom,ms),refl_V(mcom,ms),
      &      issur_self_shd(mcom,ms), proang_s(mcom,mcfc,ms),
      &      qtmca_ext(mcom,ms,mn)
       real RadIncDirS,RadIncDifS,transBB_S,transD_S,refl_S
+      real RadIncDifSky,RadIncDifGrd
       real transBB_V,transD_V,refl_V
       real issur_self_shd,proang_s,qtmca_ext
 
