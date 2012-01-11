056aee4e21d635e6b03583330d23ee8ed8657122
aferguso <aferguso@7d53e970-de11-0410-8a54-3d01b9da36cf>
2012-01-11 19:07:00 +0000

    This is an initial commit of code that refactors and rewrites substantial parts of the ideal hvac parsing and supervisory routines for greater flexibility and extensiblity. It adds the following features:
     1. a new, version 2 hvac file that is much easier to parse and 
        understand. 
     2. a prototype model of integrated mechanical systems for use 
        with CSA P.10 performance data
     3. added capabilities to impose casual gains from HVAC equipment on 
        zones. 
    
    Preliminary testing suggests these changes may create trace 
    differences in simulation results. Further testing to be undertaken.
    
    
    
    
    
    Former-commit-id: 6388548e5320eba11c25375a8fa5d17f7b042953

--- src/include/OffsiteUtilitiesPublic.h
+++ src/include/OffsiteUtilitiesPublic.h
@@ -22,6 +22,7 @@ C.....Named constants for fuel types
      &            iSoftWood    = 7,
      &            iWoodPellets = 8 )
 
+     
 C.....Supported end-uses
       integer iNumUses
       parameter ( iNumUses = 7 )
