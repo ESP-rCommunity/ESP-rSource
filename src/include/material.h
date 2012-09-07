618acf9fe8caeeeb0617ac7f865ced609b36f8b8
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2012-09-07 11:37:49 +0000

    - First step in enriching the data structures for common   constructions to support verbose MLC names and documentation   as well categories of constructions, date stamps and versioning. - This follows the general data pattern established for materials.   The common blocks and data structures (from material.h) are   shown below. - No code yet uses these common blocks, however over time   variables in common block MLC will be converted. - Subsequent steps:   a) draft a revised tag:data file format which supports      verbose MLC names and documentation   b) write I/O to convert existing constructions files   c) write I/O to scan the new file format   d) introduce logic to toggle between the 12 char and 32 char      names for constructions (and their attributes)   e) introduce logic to toggle the interface between short and      verbose naming   f) populate new constructions file with well-named and well-      documented (provinance) constructions   g) test new constructions in some new exemplars, learn from      this and devise an procedure for optional upgrading.
    - Note: currently surface construction attributes are 32 char
      but only the initial 12 char are used. The toggle would mostly
      allow the full width to be used in the interface and reporting.
    
    - It might be useful to add surface properties (solar, visual, IR)
      for each face of construction to limit the need to re-scan
      materials for this data. There might be other attributes that
      can be considered.
    
    From material.h:
      . . .
    C Constructions header common blocks:
          integer mlcver      ! version of the database 0 and 1 with verbose names
          integer mlccats     ! number of categories
          integer mlcdbitems  ! number of items (overall) in database (replaces NMLC)
          integer mlccatitems ! number of items in each category
          common/mlcheader/mlcver,mlccats,mlcdbitems,mlccatitems(MGCL)
    
    C Constructions categories
          character mlcdbdate*24 ! date stamp for the database
          character mlcdbdoc*248 ! documentation for the constructions file
          character mlccatname*32 ! identity/name of category
          character mlccatdoc*248  ! documentation for the category
          common/mlcheadertxt/mlcdbdate,mlcdbdoc,mlccatname(MGCL),
         &  mlccatdoc(MGCL)
    
    C Data structures for verbose names and documentation associated with MLC
          character mlcname*32     ! name of MLC
          character mlcdoc*248     ! documentation 
          character mlcoptical*32  ! associated optical properties name
          character mlcsymetric*32 ! associated name of reversed MLC or key words
                                   ! SYMMETRIC or NONSYMMERTIC
          common/mlcnamearray/mlcname(MMLC),mlcdoc(MMLC),mlcoptical(MMLC),
         &  mlcsymetric(MMLC)
    
          integer mlccatindex  ! is pointer to index of the MLC category
          common/mlcdatarray/mlccatindex(MMLC)
    
    
    
    
    Former-commit-id: dad57dbb9541f77ea4698bb6180f2ceaa0134c90

--- src/include/material.h
+++ src/include/material.h
@@ -2,7 +2,7 @@ C This header relates to materials data structures in ESP-r. It is
 C dependant on building.h and should follow building.h and esprdbfile.h so that
 C parameters will have been defined.
 
-C Materials common blocks:
+C Materials header common blocks:
       real matver         ! version of the database 1.1 basic 1.2 has gap resistances
       integer matcats     ! number of categories
       integer matdbitems  ! number of items (overall) in database
@@ -11,7 +11,7 @@ C Materials common blocks:
       common/matheader/matver,matcats,matdbitems,matcatitems(MGCL),
      &  origmatwasbin
 
-
+C Materials category commons
       character matdbdate*24 ! date stamp for the database
       character matdbdoc*248 ! documentation for the materials database
       character matcatname*32 ! identity/name of category
@@ -29,8 +29,7 @@ C Otherwise it is a user supplied short phrase (spaces are allowed).
 C The variable matopaq indicates whether the material
 C was opaque/transparent and/or a gas (various types).
 C 'o' is opaque, 't' is transparent, '-' imported from legacy
-C 'g' is gas (data+temp correction), 'h' is gas (data at 4 temps)
-C 'a' is air (fixed resistances (not yet available))
+C 'g' is a gap resistance layer, 'h' depreciated gas
       character matopaq*1  ! tag for opaque/transparent/gas
       common/matnamearray/matname(MGIT),matdoc(MGIT),matopaq(MGIT)
 
@@ -95,10 +94,39 @@ C materials database. To assist in recovering materials for which there is
 C no legacy index (-99) this text is used.  This array is filled when the
 C MLC database is scanned and should be updated when new materials are selected.
 C The string buffer DESC is included in MLCTXT so that MLC is all data.
+
+C << todo increase DESC buffer to 124 char >>
       character LAYDESC*124  ! first 124 char of layer text
       character DESC*48 ! string which holds 12 char name, 4 char opaque tag, 
                         ! 12 char optical tag and SYMMETRIC or NONSYMMETRIC tag
       common/MLCTXT/LAYDESC(MMLC,ME),DESC(MMLC)
+
+C Constructions header common blocks:
+      integer mlcver      ! version of the database 0 and 1 with verbose names
+      integer mlccats     ! number of categories
+      integer mlcdbitems  ! number of items (overall) in database (replaces NMLC)
+      integer mlccatitems ! number of items in each category
+      common/mlcheader/mlcver,mlccats,mlcdbitems,mlccatitems(MGCL)
+
+C Constructions categories
+      character mlcdbdate*24 ! date stamp for the database
+      character mlcdbdoc*248 ! documentation for the materials database
+      character mlccatname*32 ! identity/name of category
+      character mlccatdoc*248  ! documentation for the category
+      common/mlcheadertxt/mlcdbdate,mlcdbdoc,mlccatname(MGCL),
+     &  mlccatdoc(MGCL)
+
+C Data structures for verbose names and documentation associated with MLC
+      character mlcname*32     ! name of MLC
+      character mlcdoc*248     ! documentation 
+      character mlcoptical*32  ! associated optical properties name
+      character mlcsymetric*32 ! associated name of reversed MLC or key words
+                               ! SYMMETRIC or NONSYMMERTIC
+      common/mlcnamearray/mlcname(MMLC),mlcdoc(MMLC),mlcoptical(MMLC),
+     &  mlcsymetric(MMLC)
+
+      integer mlccatindex  ! is pointer to index of the MLC category
+      common/mlcdatarray/mlccatindex(MMLC)
 
 C Data structures associated with multi-layer constructions and their
 C references to materials (index within materials database).
