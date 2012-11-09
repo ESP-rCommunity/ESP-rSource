587694b6516ea158189acaa6e1af9dc88e49fef2
aferguso <aferguso@7d53e970-de11-0410-8a54-3d01b9da36cf>
2012-11-09 18:15:55 +0000

    This commit reverts changeset 8802.
    Former-commit-id: 54a59ba89574425258e8b895afea1f3815e917b4

--- src/include/hvac_common.h
+++ src/include/hvac_common.h
@@ -1,44 +1,5 @@
 c Common for hvac system input data. These variables are used for hvac systems
 c modeled ideally
-
-C      integer iFurnaceTokenCount
-C      parameter (iFurnaceTokenCount = 7)
-C      integer iIMSTokenCount
-C      parameter (iIMSTokenCount = 1)
-C      integer iBaseboardTokenCount
-C      parameter (iBaseboardTokenCount = 4)
-C      integer iHPTokenCount
-C
-C      parameter (iHPTokenCount = 23)
-C      integer iMAXTokenCount
-C      parameter (iMAXTokenCount = iHPTokenCount)
-
-
-      common/HVAC_parsing/iHVACFileVersion,
-     &bParseError,
-     &cFurnaceTokenNames,
-     &cBaseboardTokenNames(iBaseboardTokenCount),
-     &cP10_IMSTokenNames(iP10_IMSTokenCount),
-     &cHPTokenNames(iHPTokenCount),
-     &fFurnaceTokenVals(max_sys,iFurnaceTokenCount),
-     &fBaseboardTokenVals(max_sys,iBaseboardTokenCount),
-     &fP10_IMSTokenVals(max_sys,iP10_IMSTokenCount),
-     &fHPTokenVals(max_sys,iHPTokenCount),
-     &iDataCount(max_sys),
-     &cTokens(max_sys,iMAXTokenCount),
-     &fTokenVals(max_sys,iMAXTokenCount),
-     &iUnit_Function(max_sys), 
-     &bHVACVersion2
-     
-
-      character*72 cFurnaceTokenNames(iFurnaceTokenCount),
-     &             cBaseboardTokenNames,
-     &             cP10_IMSTokenNames, cHPTokenNames, cTokens
-      real fFurnaceTokenVals, fBaseboardTokenVals, fP10_IMSTokenVals,
-     &     fHPTokenVals, fTokenVals
-      integer iUnit_Function
-      logical bHVACVersion2
-
       common/HVAC_INPUT_DATA/site_altitude, num_hvac_sys,
      &ihvac_type(max_hvac_types), ipriority(max_sys),
      &isys_type(max_hvac_vars), num_sys_zones(max_sys),
@@ -53,84 +14,22 @@ C      parameter (iMAXTokenCount = iHPTokenCount)
      &var_flow_rate(max_sys,mcom),
      &var_fan_power(max_sys,mcom),
      &sys_zone_cap_frac_initial(max_sys,mcom),
-     &sys_zone_cap_frac_ret_air(max_sys,mcom),
-     &cSys_Name(max_sys), iSys_Function(max_sys), iSys_Backup(max_sys),
-     &cSys_Backup(max_sys),bFan_AutoSized(max_sys),
-     &bFan_Auto_AutoSized(max_sys), bFan_Rated_AutoSized(max_sys)
+     &sys_zone_cap_frac_ret_air(max_sys,mcom)
 
       common/HVAC_fuel_type/iFuel_type(max_sys)
-
-Common for fan control loops: 
-      integer max_fan_loops                  ! Number of loops permitted
-      parameter  ( max_fan_loops = max_sys ) 
-      integer iFanControlLoop(max_sys)       ! Index indicating which 
-                                             ! loop a given system is on
-      
-      real fFanSysPLR_lo(max_sys)            ! Fraction that a system 
-                                             ! needs the fan on for 
-                                             ! in low-speed (0-1)
-                                             
-      real fFanSysPLR_hi(max_sys)            ! Fraction that a system 
-                                             ! needs the fan on for 
-                                             ! in high-speed (0-1)
-                                             
-      real fFanSysPower_off(max_sys)         ! Fan power consumption in 
-                                             ! off mode. (W)
-      
-      real fFanSysPower_lo(max_sys)          ! Fan power consumption in 
-                                             ! low-speed (W)
-      
-      real fFanSysPower_hi(max_sys)          ! Fan power consumption in 
-                                             ! high-speed (W)
-                                             
-      logical bSysHasFanCtlLoop(max_sys)     ! Flag indicating system 
-                                             ! is connected to a fan 
-                                             ! control loop 
-      
-      logical bFanCtlLoopActive(max_fan_loops) ! Flag indicating a fan conrol 
-                                               ! loop is active 
-      
-      common/HVAC_Fan_Control/iFanControlLoop,
-     &                        fFanSysPLR_hi,
-     &                        fFanSysPLR_lo,
-     &                        fFanSysPower_off,
-     &                        fFanSysPower_lo,
-     &                        fFanSysPower_hi,
-     &                        bSysHasFanCtlLoop,
-     &                        bFanCtlLoopActive 
-      
-      
-      
-C Declare miscellaneous parameter storage array 
-      common/HVAC_misc_parameters/
-     &bMiscTokenValsDefined,
-     &iMiscParamCount,
-     &cMiscTokens,
-     &cMiscVals
-      logical bMiscTokenValsDefined(max_sys)
-      character*72 cMiscTokens(max_sys,iMAXTokenCount)
-      character*72 cMiscVals(max_sys,iMAXTokenCount)
-      integer iMiscParamCount(max_sys)
-
-
 
 c Declare integer values in HVAC_INPUT_DATA common
       INTEGER num_hvac_sys, ihvac_type, ipriority, isys_type,
      &num_sys_zones, ifan_operation, ifan_position, ifan_position_r,
      &isys_zone_num, iduct_sys_flag, icontrol_function, iFuel_type,
-     &iDataCount,
-     &iHVACFileVersion, iSys_Function, iSys_Backup,iZonecontrol
-     
 
 C....flag for zone control (0 = no zone control, 1 = zone control )
+     &izonecontrol
 
 c Declare real parameters in HVAC_INPUT_DATA common
-      REAL 
-     &site_altitude, ss_capacity, flow_rate, flow_rate_r,
+      REAL site_altitude, ss_capacity, flow_rate, flow_rate_r,
      &fan_power, fan_power_auto, fan_power_r, draft_fan_power,
      &sys_zone_cap_frac, 
-
-      
 
 C....Variable flow rate [m3/s] as a function of the number of calling zones
      &var_flow_rate,
@@ -147,11 +46,6 @@ C....initial values for calculations of future timestep capacity fractions.
 C....Return air zone capacity fractions. These are fixed fractions that are
 C....indpendent of zoning calls. 
      &sys_zone_cap_frac_ret_air
-
-      logical bParseError, bFan_AutoSized, bFan_Auto_AutoSized,
-     &        bFan_Rated_AutoSized
-
-      character*72 cSys_Name,cSys_Backup
 
 c Common for time step computed parameters of HVAC system
 
@@ -173,11 +67,10 @@ C.....flags to keep track of zone heating/cooling calls
 c Common for underheated cooling
 
        common/UNDER_HEAT_COOL_HOURS/ihrs_under_heat(max_sys),
-     &                              ihrs_under_cool(max_sys)
+     &                             ihrs_under_cool(max_sys)
 
 c Declare integer in UNDER_HEAT_COOL_HOURS common
       INTEGER ihrs_under_heat, ihrs_under_cool
-
 
 C.....Commons for storing HVAC system names, lengths
       common/HVAC_H3kNames/iHVACTypeLen,cHVACTypes
@@ -226,47 +119,3 @@ C.....day/period information for Conventional Cooling
      &        iend_hour_out_air_dayperiod
      
       REAL day_period_out_air_flow
-
-
-C Common for additional "casual" gains associated with HVAC systems ---
-C that is, additional heat trasnfer not accounted for in the zone's
-C ideal controls. These amounts include the resulting 'defecit'
-C occuring when the HVAC system cannot provide the heat transfer
-C injected into the space by the ideal controls. 
-
-      common/HVAC_additional_gains/bIdealHVACSysGain(max_sys),
-     &                             fIdealHVACSysAddGain(max_sys),
-     &                             fIdealHVACSysConGainFrac(mcom),
-     &                             fIdealHVACSysRadGainFrac(mcom),
-     &                             fIdealHVACSysLatGainFrac(mcom),
-     &                             bIdealHVACZoneGain(mcom),
-     &                             fIdealHVACZoneConGain(mcom),
-     &                             fIdealHVACZoneRadGain(mcom),
-     &                             fIdealHVACZoneLatGain(mcom)
-
-
-C Flag indicating presense of additional gains from idealized hvac
-C systems
-      logical bIdealHVACSysGain
-
-C Total gain from a HVAC system
-      real fIdealHVACSysAddGain
-
-C HVAC system conective/latent/radiant fractions
-
-      real fIdealHVACSysConGainFrac,
-     &     fIdealHVACSysRadGainFrac,
-     &     fIdealHVACSysLatGainFrac
-
-
-C Logical flag indicating if zone has associated gains from idealized
-C hvac components.
-      logical bIdealHVACZoneGain
-
-C Total gain into a zone from all HVAC systems (convective,
-C radiant and latent gains)
-      real fIdealHVACZoneConGain,
-     &     fIdealHVACZoneRadGain,
-     &     fIdealHVACZoneLatGain
-
-
