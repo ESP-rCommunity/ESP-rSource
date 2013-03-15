4a6a3ff8280312dbdd5966efbde861529c458868
ageissler <ageissler@7d53e970-de11-0410-8a54-3d01b9da36cf>
2013-03-15 15:09:20 +0000

    Merged revision(s) 9095-9139 from branches/development_branch:
    Minor conflicts solved manually.
    
    Compile and run of prj O.K. on Cygwin.
    
    Test model ungraciously crashes at approx. 65 % of the way. This commit as code save / transfer (check OS X first, then start debugging).
    
    
    
    
    Former-commit-id: bec2c79f659ace66d3c3ef54a8dcaf9756cc520a

--- src/validation/benchmark/CFD/Models/dfs_basic.sh
+++ src/validation/benchmark/CFD/Models/dfs_basic.sh
@@ -8,20 +8,18 @@
 DFS=$1
 model=$2
 #
-rm -f CFD_mon TECend.DAT
+rm -f CFD_mon TECp_1.dat
 # 
 time $DFS -mode text -file $model".dfd" 1> $model".out" 2> $model".time" << XYZ 
 d
 a
 e
-d
-
+e
 -
 -
 n
 y
-y
 XYZ
 mv CFD_mon $model".mon"
-mv TECend.DAT $model".TEC"
+mv TECp_1.dat $model".TEC"
 rm -f tmp_DFS_1 CFD_new_phi
