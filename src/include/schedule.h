2d0e250455d2a696eede9e06556d9b5d1d853c57
ageissler <ageissler@7d53e970-de11-0410-8a54-3d01b9da36cf>
2013-03-22 14:23:23 +0000

    Merged revision(s) 9140-9158 from branches/development_branch:
    Various conflicts in eroper.F, partially resolved using "mine" (added selection options for casual gain ventilation schemes).
    Conflicts in prjfmk.F, resolved using "theirs", however, resetting sensible and latent heat gain to my values.
    
    Commented out "write(6,*)" in eroper.F, line 564 (probably debugging residual).
    
    Compile and run on Cygwin O.K.
    
    Test model give results as expected (however does not use casual gains extensively).
    
    
    
    Former-commit-id: 584b1d2d8e3e692f9067e69be6416e84f606fc76

--- src/include/schedule.h
+++ src/include/schedule.h
@@ -54,9 +54,10 @@ C How many of each dispersed demands for each gaintype/daytype.
       integer idmdver   ! version of the demands file.
                         ! idmdver =0 standard, idmdver =1 with header
 
-C Version of zone operations file. ip3ver=0 legacy, =1 sorted with header,
-C =2 with more than 3 day types allowed and periods sorted,
-C =3 with key words for casual gain types.
+C Version of zone operations file. ip3ver=0 legacy, =1.0 (1) sorted with header,
+C =2.0 (2) with more than 3 day types allowed and periods sorted,
+C =2.1 (21) with key words for casual gain types and separate documentation,
+C =3 (30) with tag:data format (not yet fully implemented).
       integer ip3ver
       common/p3ver/ip3ver(MCOM)
 
@@ -65,12 +66,23 @@ C =3 with key words for casual gain types.
       character ctlstr*24    ! summary of air control scheme
       COMMON/P1/oprdesc(MCOM),ventdesc(MCOM),ctlstr(MCOM,MDTY)
 
-      character lodlabel*6  ! use label for each zone casual gain
-      common/loadlabel/lodlabel(mcom,MGTY)
+      character lodlabel*12   ! user label for each zone casual gain slot
+      character caskeytype*16 ! key word attribute of casual gain type (to
+                              ! replace overloaded icgt array below). Keys
+                              ! are listed in eroper.F
+      common/loadlabel/lodlabel(mcom,MGTY),caskeytype(mcom,MGTY)
+
+      integer lodslot     ! which slot (for pointer from period data lines 1st
+                          ! index) to indicate which casual slot, if zero
+                          ! the slot not used
+      integer lodatr1     ! 1st attribute associated with a caskeytype
+      integer lodatr2     ! 2nd attribute associated with a caskeytype
+      common/loadkey/lodslot(mcom,MGTY),lodatr1(mcom,MGTY),
+     &               lodatr2(mcom,MGTY)
 
 C Users can track a number of different casual gains up to MGTY and ICGT 
 C is this index for each period in each calendar day type. Older files 
-C implied a purpose to this index
+C implied a purpose to this index. << this is being depreciated >>
 C 1=occupancy W, 2=lighting W, 3=small power W, 4=other W, 5=external electrical
 C -1=floor area/person -2 Light W/m2 -3=small power W/m2
       integer ICGT  ! position within array of casual gains (overloaded see above)
@@ -89,12 +101,17 @@ C -1=floor area/person -2 Light W/m2 -3=small power W/m2
 
       integer NCAS       ! number casual gain periods for each daytype
       integer ICGS,ICGF  ! start & finish hours for each casual day:period
+      integer ICGUnit    ! unit for sensible day:period 0 is W, 1 is W/m2,
+                         ! 2 is m2/p assumes 95W sensible & 45W latent),
+                         ! 3 is W/object (future feature), 4 is Met (metabolic
+                         ! rate as per CIBSE Guide A table 1.4 future feature)
       real CMGS,CMGL     ! sensible and latent magnitude for each casual 
                          ! day:period, units depend on value of ICGT
       real RADC,CONC     ! radiant and convective fraction (0.0 - 1.0) for
                          ! each casual gain day:period
       COMMON/P3N/NCAS(MDTY),ICGS(MDTY,MC),ICGF(MDTY,MC),
-     &           CMGS(MDTY,MC),CMGL(MDTY,MC),RADC(MDTY,MC),CONC(MDTY,MC)
+     &  ICGUnit(MDTY,MC),CMGS(MDTY,MC),CMGL(MDTY,MC),RADC(MDTY,MC),
+     &  CONC(MDTY,MC)
 
 C NOTE: P2CTL should be extended to support controls for each day type.
       integer ITCTL    ! index of air flow control (see above)
