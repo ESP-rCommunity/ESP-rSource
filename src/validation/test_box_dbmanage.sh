3d38b70465b56919b3733bb8d1b8a2b42717f45f
jhand <jhand@7d53e970-de11-0410-8a54-3d01b9da36cf>
2013-06-12 12:03:57 +0000

    - Update the automated test script test_box_dbmanage.sh   to take a newly created test_box and update the database   references to poit to local material and MLC files. - This was used to confirm how well prj driven in text   mode would respond to database management tasks. Additional   scripts will be added to define a simple zone next.
    Testing
    - Several rounds of interactive tests and invocations
      of the script and checking that the model files were
      as expected.
    
    
    
    
    Former-commit-id: 5cdeadce61261d20bd67452e059a4fb21a189a6e

--- src/validation/test_box_dbmanage.sh
+++ src/validation/test_box_dbmanage.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# this takes model in the folder test_box and scans it
-# to establish databases and uses a local copy of weather
+# This takes model in the folder test_box and scans it
+# to establish databases and uses a local copy of materials
+# and constructions.
 # the command line needs to include path to esp-r distribution
 VERSION=$1
 cd test_box/cfg
 pwd
-$VERSION/prj -file test_box.cfg<<XXX
+$VERSION/prj -mode text -file test_box.cfg<<XXX
 b # database management
 - 
 s # save as
@@ -14,11 +15,14 @@ test_box_a
 b
 c # materials
 d # copy default
+
 -
 e # MLC
 d # copy default
+
 -
 -
 -
+r
 -
 XXX
