fe36f80d4e84eab1ad7e4ae5e162c3ccc92a3d67
Jon Hand <jon@mae-esru-jon>
2017-01-17 14:17:46 +0000

    - Update Install scripts to detect MSYS2 running under W10. - Add script to copy critical dll files to C:/ESRU/esp-r/bin   when doing a native Windows compile.

---
+++ copy_dll_to_c_esru_esp-r_bin.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+#-----------------------------------------------------------
+# Run this to copy essential dll files into C:/ESRU/esp-r/bin
+# if you are building ESP-r within a MSYS2 environment.
+cp C:/msys64/mingw64/bin/libgcc_s_seh-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgdk-win32-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgfortran-3.dll  C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgdk_pixbuf-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgtk-win32-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libglib-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libatk-1.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpango-1.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgdk-win32-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libcairo-2.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpangocairo-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpangoft2-1.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpangowin32-1.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgio-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgobject-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgmodule-2.0-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libwinpthread-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libquadmath-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libintl-8.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpng16-16.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpixman-1-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libpcre-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libffi-6.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libiconv-2.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libfreetype-6.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libfontconfig-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libexpat-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/zlib1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libbz2-1.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libharfbuzz-0.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libgraphite2.dll C:/ESRU/esp-r/bin
+cp C:/msys64/mingw64/bin/libstdc++-6.dll C:/ESRU/esp-r/bin
