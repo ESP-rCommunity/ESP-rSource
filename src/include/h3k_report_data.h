7e40ef2454e9a8cfb4775cb78e94616d986787e9
Adam Wills <Willsa@NRC-021169.ds.nrc.ca>
2018-02-27 14:29:24 -0500

    Explicit energy consumption of ventilatiom fans with no heat recovery has been added to the h3k reports. Compiles and runs on Cygwin.

--- src/include/h3k_report_data.h
+++ src/include/h3k_report_data.h
@@ -5,7 +5,9 @@ C These flags are used for integrating output variables for quick run (seasonal)
       logical bFirst_call_fan
       logical bFirst_call_hrv
 	  logical bFirst_call_erv		! 27-08-2014: Patrice Pinel: Added to report Energy revovery ventilation
+	  logical bFirst_call_noHR		! 27-02-2018: Patrice Pinel: Added to report Energy no HR ventilation
       logical bFirst_call_dhw
       common/h3k_reports_flags/bFirst_call_bld, bFirst_call_hvac,
      &                         bFirst_call_fan, bFirst_call_hrv,
-     &                         bFirst_call_erv, bFirst_call_dhw
+     &                         bFirst_call_erv, bFirst_call_dhw,
+     &                         bFirst_call_noHR
